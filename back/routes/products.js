c2fd890cd999b851c3242a82dae2d3276593e9c3
Cristianzz10 <u20162152956@usco.edu.co>
2022-10-24 22:40:34 -0500

    Clase 11 de octubre - CRUD

--- back/routes/products.js
+++ back/routes/products.js
@@ -1,8 +1,8 @@
 const express=require("express")
 const router=express.Router();
 
-const {getProducts} = require("../controllers/productsController") //Traemos la respuesta json desde el controlador
+const {getProducts, newProduct} = require("../controllers/productsController") //Traemos la respuesta json desde el controlador
 
 router.route('/productos').get(getProducts) //Establecemos desde que ruta queremos ver el getProducts
-
+router.route('/productos/nuevo').post(newProduct); //establecemos la ruta
 module.exports=router;
